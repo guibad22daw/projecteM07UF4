a3c059bb8a5d81ffc157feaa8df594d4dabb77c1
guibad22daw <guibad22daw@proton.me>
2022-10-17 16:50:53 +0200

    iniciarJoc acabat?

--- RESTful i Nodejs - Express/RockPaperScissors.js
+++ RESTful i Nodejs - Express/RockPaperScissors.js
@@ -10,47 +10,35 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()) // per analitzar les peticions HTTP que portin JSON al body
 
-// let alumnes = [
-//     {codi:1, nom:'SERGI',nota:5},
-//     {codi:2, nom:'ANNA',nota:6},
-//     {codi:3, nom:'JOAN',nota:4}
-// ];
-
-let codiPartides = [
-    { "codi": '0', "nom": 'PROVA', "estatPartida": 'Acabada' }
+let llistaPartides = [
+    { codi: 0, nom: 'PROVA', estatPartida: 'Acabada' }
 ];
 
-let codis = [];
+let codis = [0];
 
 app.get('/', (req, res) => res.send('hola'));
 
 app.post('/api/iniciarJoc/:codiPartida', (req, res) => {
-    let partida = [];
+    let partida = [], codi = parseInt(req.params.codiPartida);
     partida.length = 0;
-    for (let i of codiPartides) {
-        if (i.codi == req.params.codiPartida) {
-            res.send("Codi de partida ja existent.");
-        } if (!req.body.nom) {
-            res.send("Si us plau introdueix un nom.")
-
-        } else {
-            partida = { "codi": parseInt(req.params.codiPartida), "nom": req.body.nom, "estatPartida": 'A punt de començar.' };
-        }
-    }
-    codiPartides.push(partida);
-    codis.push(req.params.codiPartida);
-    console.log(codiPartides);
+    
+    if (codis.includes(codi)) res.send("Codi de partida ja existent.");
+    else if (!req.body.nom) res.send("Si us plau introdueix un nom.");
+    else if (!req.params.codiPartida || isNaN(codi)) res.send("Si us plau introdueix un codi vàlid.");
+    else {
+        partida = { codi: parseInt(req.params.codiPartida), nom: req.body.nom, estatPartida: 'A punt de començar.' };
+        llistaPartides.push(partida);
+        codis.push(codi);
+        res.send(`Partida creada. Número de partida: ${req.params.codiPartida}`);
+    }    
+    console.log(llistaPartides);
     console.log(codis);
-    res.send(codiPartides);
 
 });
 
 app.get('/api/consultarEstatPartida/codiPartida', (req, res) => {
-    let partida = codiPartides.find(a => a.codi === parseInt(req.body.codi));
-    for (let i of codiPartides) codis.push(i.codi);
-    for (let i of codiPartides) {
-        let codis = [];
-        codis.push(i.codi);
+    let partida = codis.find(a => a.codi === parseInt(req.body.codi));
+    for (let i of codis) {
         if (i.codi == req.body.codi) {
             res.send(partida);
         } else if (!req.body.codi) res.send("Introdueix un codi.");
@@ -61,13 +49,12 @@ app.get('/api/consultarEstatPartida/codiPartida', (req, res) => {
 });
 
 let jugador1 = 0, jugador2 = 0;
-let pedra1 = 0, paper1 = 0, tisores1 = 0, pedra2 = 0, paper2 = 0, tisores2 = 0;
 
 app.put('/api/moureJugador/codiPartida/jugador/tipusMoviment', (req, res) => { // /api/moureJugador/123/1/tisores
     let partidaActual = { "codi": req.body.codi, "jugador": req.body.jugador, "tipusMoviment": req.body.tipusMoviment };
     // ASSIGNACIONS
-    let partida = codiPartides.find(a => a.codi === parseInt(req.body.codi));
-    for (let i of codiPartides) {
+    let partida = codis.find(a => a.codi === parseInt(req.body.codi));
+    for (let i of codis) {
         if (i.codi == req.body.codi) {
             if (req.body.jugador == 1 && req.body.tipusMoviment == "pedra") {
                 jugador1 = "pedra";
