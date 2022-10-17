a64d8abcddf86db05268afbd81f8fce75a5df64f
guibad22daw <guibad22daw@proton.me>
2022-10-17 15:47:59 +0200

    Codi sense ús esborrat.

--- RESTful i Nodejs - Express/RockPaperScissors.js
+++ RESTful i Nodejs - Express/RockPaperScissors.js
@@ -20,6 +20,8 @@ let codiPartides = [
     { "codi": '0', "nom": 'PROVA', "estatPartida": 'Acabada' }
 ];
 
+let codis = [];
+
 app.get('/', (req, res) => res.send('hola'));
 
 app.post('/api/iniciarJoc/:codiPartida', (req, res) => {
@@ -36,14 +38,15 @@ app.post('/api/iniciarJoc/:codiPartida', (req, res) => {
         }
     }
     codiPartides.push(partida);
+    codis.push(req.params.codiPartida);
     console.log(codiPartides);
+    console.log(codis);
     res.send(codiPartides);
 
 });
 
 app.get('/api/consultarEstatPartida/codiPartida', (req, res) => {
     let partida = codiPartides.find(a => a.codi === parseInt(req.body.codi));
-    let codis = [];
     for (let i of codiPartides) codis.push(i.codi);
     for (let i of codiPartides) {
         let codis = [];
@@ -69,181 +72,41 @@ app.put('/api/moureJugador/codiPartida/jugador/tipusMoviment', (req, res) => { /
             if (req.body.jugador == 1 && req.body.tipusMoviment == "pedra") {
                 jugador1 = "pedra";
                 console.log("Pedra1.");
-            }else if (req.body.jugador == 2 && req.body.tipusMoviment == "pedra") {
+            } else if (req.body.jugador == 2 && req.body.tipusMoviment == "pedra") {
                 jugador2 = "pedra"
                 console.log("Pedra2.");
-            }else if (req.body.jugador == 1 && req.body.tipusMoviment == "paper") {
+            } else if (req.body.jugador == 1 && req.body.tipusMoviment == "paper") {
                 jugador1 = "paper";
                 console.log("Paper1.");
-            }else if (req.body.jugador == 2 && req.body.tipusMoviment == "paper") {
+            } else if (req.body.jugador == 2 && req.body.tipusMoviment == "paper") {
                 jugador2 = "paper";
                 console.log("Paper2.");
-            }else if (req.body.jugador == 1 && req.body.tipusMoviment == "tisores") {
+            } else if (req.body.jugador == 1 && req.body.tipusMoviment == "tisores") {
                 jugador1 = "tisores";
                 console.log("Tisores1.");
-            }else if (req.body.jugador == 2 && req.body.tipusMoviment == "tisores") {
+            } else if (req.body.jugador == 2 && req.body.tipusMoviment == "tisores") {
                 jugador2 = "tisores";
                 console.log("Tisores2.");
             }
-            
-            if(jugador1 === jugador2){
+
+            if (jugador1 === jugador2) {
                 res.send("Empat.")
-            }else{
-                if(jugador1 == "pedra" && jugador2 == "paper"){
-                    res.send("Guanya jugador 2.");
-                }
-                else if(jugador1 == "pedra" && jugador2 == "tisores"){
-                    res.send("Guanya jugador 1.");
-                }
-                else if(jugador1 == "paper" && jugador2 == "pedra"){
-                    res.send("Guanya jugador 1.");
-                }
-                else if(jugador1 == "paper" && jugador2 == "tisores"){
-                    res.send("Guanya jugador 2.");
-                }                
-                else if(jugador1 == "tisores" && jugador2 == "paper"){
-                    res.send("Guanya jugador 1.");
-                }
-                else if(jugador1 == "tisores" && jugador2 == "pedra"){
-                    res.send("Guanya jugador 2.");
-                }                               
+            } else {
+                if (jugador1 == "pedra" && jugador2 == "paper") res.send("Guanya jugador 2.");
+                else if (jugador1 == "pedra" && jugador2 == "tisores") res.send("Guanya jugador 1.");
+                else if (jugador1 == "paper" && jugador2 == "pedra") res.send("Guanya jugador 1.");
+                else if (jugador1 == "paper" && jugador2 == "tisores") res.send("Guanya jugador 2.");
+                else if (jugador1 == "tisores" && jugador2 == "paper") res.send("Guanya jugador 1.");
+                else if (jugador1 == "tisores" && jugador2 == "pedra") res.send("Guanya jugador 2.");
             }
 
-            console.log(jugador1,jugador2);
+            console.log(jugador1, jugador2);
         } else if (!req.body.codi) res.send("Introdueix un codi vàlid.");
     }
-    // if (req.body.jugador > 2) res.send("Escull si ets el jugador 1 o 2.");
-
-    
-    // switch (key) {
-    //     case "pedra":
-    //         if ((jugador1 && pedra) && (jugador2 && paper)) res.send("Jugador 2 guanya.");
-    //         else if ((jugador1 && pedra) && (jugador2 && tisores)) res.send("Jugador 1 guanya.");
-    //         break;
-        
-    //     case "paper":
-    //         if ((jugador1 && paper) && (jugador2 && tisores)) res.send("Jugador 2 guanya.");
-    //         else if ((jugador1 && paper) && (jugador2 && pedra)) res.send("Jugador 1 guanya.");
-    //         break;
-        
-    //     case "tisores":
-    //         if ((jugador1 && tisores) && (jugador2 && pedra)) res.send("Jugador 2 guanya.");
-    //         else if ((jugador1 && tisores) && (jugador2 && paper)) res.send("Jugador 1 guanya.");
-    //         break;
-    // }
-
-    // console.log(jugador1,tisores,jugador2,paper,pedra);
-    // if ((jugador1 == 1 && pedra == 1) && (jugador2 == 1 && paper == 1)) {
-    //     console.log("Guanya jugador 2.");
-    //     res.send("Guanya jugador 2.");
-
-    // }
-    // else if ((jugador1 == 1 && pedra == 1) && (jugador2 == 1 && tisores == 1)) {
-    //     console.log("Guanya jugador 1.");
-    //     res.send("Guanya jugador 1.");
-        
-    // }    
-    // else if ((jugador1 == 1 && paper == 1) && (jugador2 == 1 && pedra == 1)) {
-    //     console.log("Guanya jugador 1.");
-    //     res.send("Guanya jugador 1.");
-        
-    // }
-    // else if ((jugador1 == 1 && paper == 1) && (jugador2 == 1 && tisores == 1)) {
-    //     console.log("Guanya jugador 2.");
-    //     res.send("Guanya jugador 2.");
-        
-    // }   
-    // else if ((jugador1 == 1 && tisores == 1) && (jugador2 == 1 && pedra == 1)) {
-    //     console.log("Guanya jugador 2.");
-    //     res.send("Guanya jugador 2.");
-        
-    // }
-    
-    // else if (jugador1 == 1 && tisores == 1 && jugador2 == 1 && paper == 1) {
-    //     res.send("Guanya jugador 1.");
-    // }     
-     
-    //  if(req.body.tipusMoviment == "pedra") pedra = 1;
-    //  if(req.body.tipusMoviment == "paper") paper = 1;
-    //  if(req.body.tipusMoviment == "tisores") tisores = 1;
-    //  if(jugador1 && pedra && jugador2 && paper){
-    //      res.send("Guanya el jugador 2.");
-    //  }
-    //  else if(jugador1 && pedra && jugador2 && tisores){
-    //      res.send("Guanya el jugador 1.");
-    //  }
-    //  else if(jugador1 && pedra && jugador2 && pedra){
-    //      res.send("Empat.");
-    //  }        
 });
 
 app.delete('/api/acabarJoc/codiPartida', (req, res) => {
     res.send(alumnes);
 });
 
-// app.get('/api/alumnes/:codi', (req, res)=>{
-//     let alumne = alumnes.find(a =>a.codi===parseInt(req.params.codi));
-//     if (!alumne) res.status(404, 'error');
-//     res.send(alumne);
-// });
-
-// app.get('/api/aprovats', (req, res)=>{
-//     let aprovats = [];
-//     for(let i=0; i<alumnes.length; i++){ 
-//         if(alumnes[i].nota >= 5) {
-//             let alumne = alumnes[i];
-//             aprovats.push(alumne);
-//         }
-//     }
-//     res.send(aprovats);
-// });
-
-// app.get('/api/mitjana', (req, res)=>{
-//     let sumaNotes = 0;
-//     let numAlumnes = 0;
-//     for(let i=0; i<alumnes.length; i++){ 
-//         sumaNotes = sumaNotes + alumnes[i].nota;
-//         numAlumnes = numAlumnes + i;
-//     }
-//     let mitjana = sumaNotes / numAlumnes;
-//     res.send(`La mitjana de les notes de tots els alumnes es ${mitjana.toFixed(2)}.`);
-// });
-
-// app.put('/api/canviarNota/:notaAntiga/:notaNova', (req, res) => {
-//     for (let i of alumnes){
-//         console.log(i);
-//             if (i.nota == req.params.notaAntiga){
-//                 i.nota = parseInt(req.params.notaNova);
-//             } 
-//     }
-//     res.send(alumnes);
-// });
-
-// app.post('/api/alumnes', (req, res)=>{
-//     console.log(req.body.codi);
-//     let alumne={codi: parseInt(req.body.codi), nom: req.body.nom, nota: req.body.nota };
-
-//     alumnes.push(alumne);
-//     res.send(alumnes);
-// });
-
-// app.delete('/api/alumnes/:codi', (req, res)=>{
-//     var alumne = alumnes.find(a =>a.codi===parseInt(req.params.codi));
-//     var index =alumnes.indexOf(alumne);
-
-//     if(alumne){
-//         alumnes.splice(index, 1);
-//         res.send(alumnes);
-//     }
-// });
-
-// app.put('/api/alumnes/:codi', (req, res)=>{
-//     let nouAlumne={codi: req.body.codi, nom: req.body.nom, nota: req.body.nota };
-//     let alumne = alumnes.find(a =>a.codi===parseInt(req.params.codi));
-//     let index =alumnes.indexOf(alumne);
-
-//     alumnes[index]=nouAlumne;
-//     res.send(alumnes)
-// });
-
-app.listen(3002, () => console.log('Servidor iniciat.'));
+app.listen(3000, () => console.log('Servidor iniciat.'));
